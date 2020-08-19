c0b75e59c8905e7b6ead36c29c0005294bc81dc7
GlebZhidovich <gzhidovich@gmail.com>
2020-08-19 23:31:11 +0300

    feat: add login form

--- youtube-client-app/src/app/auth/auth.module.ts
+++ youtube-client-app/src/app/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent],
-  imports: [
-	CommonModule,
-  ],
+	imports: [
+		CommonModule,
+  	MaterialModule,
+	],
+  exports: [LoginComponent],
 })
 export class AuthModule { }
