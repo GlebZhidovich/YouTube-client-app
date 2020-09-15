5e1e0a29622ad9ba2483b3b5f790d901930b3d51
GlebZhidovich <gzhidovich@gmail.com>
2020-09-15 21:55:47 +0300

    refactor: change video date pipe

--- youtube-client-app/src/app/shared/pipes/video-date.pipe.ts
+++ youtube-client-app/src/app/shared/pipes/video-date.pipe.ts
@@ -5,24 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class VideoDatePipe implements PipeTransform {
 
-  private _day: number;
-
-  public set day(publicDate: number) {
+  private dateToDay(date: number): number {
     const curDate: number = Date.now();
-    this._day = Math.round((curDate - publicDate) / 1000 / 60 / 60 / 24);
-  }
-
-  public get day(): number {
-    return this._day;
+    return Math.round((curDate - date) / 1000 / 60 / 60 / 24);
   }
 
   public transform(value: string): string {
-    this.day = new Date(value).valueOf();
+    const date: number = new Date(value).valueOf();
+    const day: number = this.dateToDay(date);
     const border: object = {
-      '#005eff': this.day < 7,
-      '#2d8000': this.day > 7 && this.day < 30,
-      '#ffff00': this.day > 30 && this.day < 180,
-      '#ff0000': this.day > 180,
+      '#005eff': day < 7,
+      '#2d8000': day > 7 && day < 30,
+      '#ffff00': day > 30 && day < 180,
+      '#ff0000': day > 180,
     };
     for (const color in border) {
       if (border[color]) {
