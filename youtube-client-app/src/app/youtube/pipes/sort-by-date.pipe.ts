8a36ab91a405f4bc56c048007272bc1d2b4f15e9
GlebZhidovich <gzhidovich@gmail.com>
2020-09-19 12:35:01 +0300

    refactor: change sort by date pipe

--- youtube-client-app/src/app/youtube/pipes/sort-by-date.pipe.ts
+++ youtube-client-app/src/app/youtube/pipes/sort-by-date.pipe.ts
@@ -7,17 +7,17 @@ import { IVideo } from '../../shared/models/search-response.model';
 export class SortByDatePipe implements PipeTransform {
 
   public transform(value: IVideo[], type: string): IVideo[] {
+    const result: IVideo[] = [...value];
     if (value && type) {
-      value
+      result
         .sort((a: IVideo, b: IVideo): number => {
           if (type.includes('up')) {
             return new Date(a.snippet.publishedAt).valueOf() - new Date(b.snippet.publishedAt).valueOf();
-          } else {
-            return new Date(b.snippet.publishedAt).valueOf() - new Date(a.snippet.publishedAt).valueOf();
           }
+          return new Date(b.snippet.publishedAt).valueOf() - new Date(a.snippet.publishedAt).valueOf();
         });
     }
-    return value;
+    return result;
   }
 
 }
